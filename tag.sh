a4cabe3577ea5c1c7b0235b1756acd82de67fd02
Jamal Khan <jamal919@gmail.com>
2022-09-21 23:03:01 +0200

    :boom: Fixes a bug in tag.sh

--- tag.sh
+++ tag.sh
@@ -6,4 +6,4 @@ docker push jamal919/telemac:v8p2r1
 
 docker build -t telemac:latest .
 docker tag telemac:latest jamal919/telemac:latest
-docker push jamal919/:latest
+docker push jamal919/telemac:latest
